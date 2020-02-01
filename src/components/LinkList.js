39270ea3817d83fc051a313e540340e02ccf6d51
Jodi N Kansagor <joedvice@gmail.com>
2020-01-31 21:35:59 -0800

    link list component

---
+++ src/components/LinkList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from './Link';
+import PropTypes from 'prop-types';
+
+const LinkList = ({ linkList }) => {
+  const linkElements = linkList.map((link, i) => (
+    <li key={i}>
+      <Link link={link.link} linkImg={link.linkImg} linkName={link.linkName} />
+    </li>
+
+  ));
+
+  return (
+    <ul>
+      {linkElements}
+    </ul>
+  );
+};
+
+LinkList.propTypes = {
+  linkList: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    linkImg: PropTypes.string.isRequired,
+    linkName: PropTypes.string.isRequired
+  }))
+}
+
+export default LinkList;
