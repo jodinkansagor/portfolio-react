c098a31fa6a5e0365473d8d1465946345940b7b4
Jodi N Kansagor <joedvice@gmail.com>
2020-02-03 08:14:47 -0800

    working on media queries

---
+++ src/components/Flickr.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Flickr = () => {
+  return (
+    <>
+      <a data-flickr-embed="true" data-footer="true" href="https://www.flickr.com/photos/biodivlibrary/albums/72157712817080583" title="Histoire naturelle, générale et particulière des céphalopodes acétabuifères viants, et fossiles : t.2 [Atlas]"><img src="https://live.staticflickr.com/65535/49434429158_322249f40d_m.jpg" width="159" height="240" alt="Histoire naturelle, générale et particulière des céphalopodes acétabuifères viants, et fossiles : t.2 [Atlas]" /></a>
+      <script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+    </>
+  );
+};
+
+export default Flickr;
--- src/containers/Container.js
+++ src/containers/Container.js
@@ -6,6 +6,7 @@ import AboutMe from '../components/AboutMe';
 import Experience from '../components/Experience';
 import ProjectList from '../components/ProjectList';
 import Footer from '../components/common/Footer';
+import Flickr from '../components/Flickr';
 
 export default class Container extends Component {
 
@@ -36,7 +37,7 @@ export default class Container extends Component {
       githubLink: 'https://github.com/jodinkansagor/daily-card-pulls'
     },
     {
-      img: '../src/assets/markov-twain.png',
+      img: '../src/assets/markov.png',
       description: 'Node based back end Twitter bot using Markov Chain technology to create predictive text sentences.',
       stack: 'Tech: Node, Express, MongoDB, Mongoose, HTML, CSS, Vanilla JS, multiple npm packages',
       siteLink: 'https://markovtwain.herokuapp.com',
@@ -46,27 +47,27 @@ export default class Container extends Component {
       img: '../src/assets/treksandtrails.png',
       description: 'Combined information from 5 different 3rd party APIs to create informative front end hike and camping search site.',
       stack: 'Tech: Vanilla JS, HTML, CSS, SQL, Postgres, Node, Express',
-      siteLink: 'https://markovtwain.herokuapp.com',
-      githubLink: 'https://github.com/textables/MarkovTwain'
+      siteLink: 'https://treksntrails.herokuapp.com',
+      githubLink: 'https://github.com/Plant-Parenthood/treks-n-tents'
     },
     {
       img: '../src/assets/magical_creatures.png',
       description: 'My spin on a typical to-do app. Create a list of magical creatures you want to collect and check them off once collected!',
       stack: 'Tech: Node, Express, Vanilla JS, HTML, CSS, SQL, Postgres',
-      siteLink: 'https://markovtwain.herokuapp.com',
-      githubLink: 'https://github.com/textables/MarkovTwain'
+      siteLink: 'https://unicorn-gathering.herokuapp.com/',
+      githubLink: 'https://github.com/jodinkansagor/to-do-list'
     },
 
     {
       img: '../src/assets/pokedex.png',
-      description: 'Curious about Pokemon? Learn more about them with this Pokedex! Utilized pokemon 3rd party api.',
+      description: 'Curious about Pokemon? Learn more about them with this Pokedex! Utilized pokemon 3rd party api to gather all the Pokemon.',
       stack: 'Tech: HTML, CSS, Vanilla JS, superagent',
       siteLink: 'https://afternoon-hamlet-35387.herokuapp.com/pokedex.html',
       githubLink: 'https://github.com/jodinkansagor/pokedex'
     },
     {
       img: '../src/assets/astro_learning.png',
-      description: 'A simple astrology learning module. Choose a sign and an astrological body and learn more about that aspect. Continuing to add data to the project.',
+      description: 'A simple astrology learning module. Choose a sign and an astrological body and learn more about that aspect. Continuing to add data to the project. Still a work in progress',
       stack: 'Tech: Vanilla JS, HTML, CSS',
       siteLink: 'https://jodinkansagor.github.io/astro-learning/',
       githubLink: 'https://github.com/jodinkansagor/astro-learning'
@@ -80,10 +81,13 @@ export default class Container extends Component {
           <Name />
           <LinkList linkList={this.linkList} />
         </section>
-        <AboutMe />
-        <Experience />
-        <ProjectList projectList={this.projectList} />
-        <Footer />
+        <section className={styles.bottomHalf}>
+          <AboutMe />
+          <Experience />
+          <ProjectList projectList={this.projectList} />
+          <Flickr />
+          <Footer />
+        </section>
       </section>
     );
   }
