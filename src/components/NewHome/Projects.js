bcb0a6af2bc9bcaa43c5a953d851a58db2cdde52
Jodi N Kansagor <joedvice@gmail.com>
2020-02-28 19:34:10 -0800

    added a couple of commas

--- src/components/NewHome/Projects.js
+++ src/components/NewHome/Projects.js
@@ -7,8 +7,8 @@ const Projects = () => (
       <h1>Things I have made: </h1>
     </section>
     <section>
-      <a href="https://coffeepluscardspdx.herokuapp.com" target="_blank">Coffee + Cards</a>
-      <a href="https://markovtwain.herokuapp.com" target="_blank">Markov Twain</a>
+      <a href="https://coffeepluscardspdx.herokuapp.com" target="_blank">Coffee + Cards,</a>
+      <a href="https://markovtwain.herokuapp.com" target="_blank">Markov Twain,</a>
       <a href="https://unicorn-gathering.herokuapp.com/" target="_blank">Treks &apos;n Trails</a>
     </section>
   </section>
